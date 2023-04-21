579c799e2809087b6fc085b01f87c25743409378
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2023-04-21 11:14:19 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <Novice.h>
+#include "Vector3D/Vector3D.h"
+#include "Mat4x4/Mat4x4.h"
+
+const char kWindowTitle[] = "LE2A_04_キクタニ_タクマ_タイトル";
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	// ライブラリの初期化
+	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	// キー入力結果を受け取る箱
+	char keys[256] = {0};
+	char preKeys[256] = {0};
+
+	Vector3D rotate(0.4f, 1.43f, -0.8f);
+	Mat4x4 rotateXMatrix = MakeMatrixRotateX(rotate.x);
+	Mat4x4 rotateYMatrix = MakeMatrixRotateY(rotate.y);
+	Mat4x4 rotateZMatrix = MakeMatrixRotateZ(rotate.z);
+	Mat4x4 rotateXYZMatrix = rotateXMatrix * rotateYMatrix * rotateZMatrix;
+
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0) {
+		// フレームの開始
+		Novice::BeginFrame();
+
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
+
+		///
+		/// ↓更新処理ここから
+		///
+
+		///
+		/// ↑更新処理ここまで
+		///
+
+		///
+		/// ↓描画処理ここから
+		///
+		MatrixScreenPrintf(0, 0, rotateXMatrix, "rotateXMatrix");
+		MatrixScreenPrintf(0, kRowheight * 5, rotateYMatrix, "rotateYMatrix");
+		MatrixScreenPrintf(0, kRowheight * 5 * 2, rotateZMatrix, "rotateZMatrix");
+		MatrixScreenPrintf(0, kRowheight * 5 * 3, rotateXYZMatrix, "rotateXYZMatrix");
+		///
+		/// ↑描画処理ここまで
+		///
+
+		// フレームの終了
+		Novice::EndFrame();
+
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
+		}
+	}
+
+	// ライブラリの終了
+	Novice::Finalize();
+	return 0;
+}
